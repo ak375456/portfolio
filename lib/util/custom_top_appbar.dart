6f833acf9af9c925c210e78cebd6bf4982098dd8
ak375456 <ak375456@gmail.com>
2025-01-11 22:26:13 +0500

    Setting up responsiveness (LayoutBuilder)

--- lib/util/custom_top_appbar.dart
+++ lib/util/custom_top_appbar.dart
@@ -5,36 +5,47 @@ class CustomTopAppbar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 70,
-      decoration: BoxDecoration(
-        color: Color.fromARGB(
-          255,
-          44,
-          44,
-          44,
-        ),
-      ),
-      child: Row(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 110.0,
-            ),
-            child: Text(
-              "Portfolio",
-              style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.w900,
-                color: Colors.white,
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > 1200) {
+          return Container(
+            height: 70,
+            decoration: BoxDecoration(
+              color: Color.fromARGB(
+                255,
+                44,
+                44,
+                44,
               ),
             ),
-          ),
-          Row(
-            children: <Widget>[],
-          ),
-        ],
-      ),
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 110.0,
+                  ),
+                  child: Text(
+                    "Portfolio",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w900,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Row(
+                  children: <Widget>[],
+                ),
+              ],
+            ),
+          );
+          ;
+        } else if (constraints.maxWidth > 800 && constraints.maxWidth <= 1200) {
+          return Text("Tablet");
+        } else {
+          return Text("Mobile");
+        }
+      },
     );
   }
 }
