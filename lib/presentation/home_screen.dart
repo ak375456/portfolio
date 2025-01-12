61b20a025554e9601af71f37c5bb8321213131e5
ak375456 <ak375456@gmail.com>
2025-01-12 14:16:04 +0500

    Top App bar for web(Desktop)

--- lib/presentation/home_screen.dart
+++ lib/presentation/home_screen.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:portfolio/util/custom_top_appbar.dart';
+import 'package:portfolio/util/cusotm_top_appbar/custom_top_appbar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          CustomTopAppbar(),
-        ],
+    return SafeArea(
+      child: Scaffold(
+        body: LayoutBuilder(
+          builder: (context, constraints) {
+            if (constraints.maxWidth > 1080 || constraints.maxHeight > 1920) {
+              return CustomTopAppbarDesktop();
+            } else {
+              return Text(
+                "Mobile",
+                style: TextStyle(color: Colors.white),
+              );
+            }
+          },
+        ),
       ),
     );
   }
