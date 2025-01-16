25464044f4b251821f8f08745213b0b5fb6969aa
ak375456 <ak375456@gmail.com>
2025-01-16 15:35:40 +0500

    Project section setting up for mobile and desktop

---
+++ lib/util/project_section/project_section_mobile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProjectSectionMobile extends StatelessWidget {
+  const ProjectSectionMobile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
