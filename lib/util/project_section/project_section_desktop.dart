3289b2c74691326b4e522fa8ed50d025b51149e6
ak375456 <ak375456@gmail.com>
2025-01-16 18:17:44 +0500

    when hover on image it will be zoomed and added click functionality it will take you to my github profile

--- lib/util/project_section/project_section_desktop.dart
+++ lib/util/project_section/project_section_desktop.dart
@@ -1,5 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ProjectSectionDesktop extends StatelessWidget {
   const ProjectSectionDesktop({super.key});
@@ -35,17 +36,67 @@ class ProjectSectionDesktop extends StatelessWidget {
 }
 
 Widget buildImageContainer(String assetPath) {
-  return Container(
-    margin: EdgeInsets.all(6.0),
-    decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(12.0),
-      image: DecorationImage(
-        image: AssetImage(assetPath),
-        fit: BoxFit.cover,
-        onError: (error, stackTrace) {
-          debugPrint('Error loading image: $assetPath');
+  return HoverableImageContainer(assetPath: assetPath);
+}
+
+class HoverableImageContainer extends StatefulWidget {
+  final String assetPath;
+
+  const HoverableImageContainer({super.key, required this.assetPath});
+
+  @override
+  _HoverableImageContainerState createState() =>
+      _HoverableImageContainerState();
+}
+
+class _HoverableImageContainerState extends State<HoverableImageContainer> {
+  bool _isHovered = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (_) => setState(() {
+        _isHovered = true;
+      }),
+      onExit: (_) => setState(() {
+        _isHovered = false;
+      }),
+      cursor: SystemMouseCursors.click,
+      child: GestureDetector(
+        onTap: () async {
+          const githubUrl = "https://github.com/ak375456";
+          if (await canLaunchUrl(Uri.parse(githubUrl))) {
+            await launchUrl(Uri.parse(githubUrl),
+                mode: LaunchMode.externalApplication);
+          } else {
+            throw 'Could not launch $githubUrl';
+          }
         },
+        child: AnimatedScale(
+          scale: _isHovered ? 1.1 : 1.0, // Zoom scale
+          duration: Duration(milliseconds: 300), // Smooth animation
+          curve: Curves.easeInOut,
+          child: Container(
+            margin: EdgeInsets.all(6.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12.0),
+              image: DecorationImage(
+                image: AssetImage(widget.assetPath),
+                fit: BoxFit.cover,
+              ),
+              boxShadow: _isHovered
+                  ? [
+                      BoxShadow(
+                        color: Colors.black.withValues(alpha: 0.3),
+                        blurRadius: 12.0,
+                        offset: Offset(0, 6),
+                      )
+                    ]
+                  : [],
+            ),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
